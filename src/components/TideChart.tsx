5cadbbd6d1f926bab34ec3436a0ed460d6bbeb66
CrossoverX <feng73440@gmail.com>
2025-12-02 06:55:43 +0800

    Update TideChart.tsx

--- src/components/TideChart.tsx
+++ src/components/TideChart.tsx
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react';
 import { Line } from 'react-chartjs-2';
 import { TideData } from '../types/tide';
-import { lunar } from "lunar-javascript";
 import { Solar } from "lunar-javascript";
 
 interface TideChartProps {
